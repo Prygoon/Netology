4f2c0dbe67660ab3534e6be108543417cd5e5f63
Qwerty <i@dont.know>
2023-01-29 02:42:30 +0300

    5-05 almost done. Need to sleep

--- SNET/5-05/scripts/5-05_1.sh
+++ SNET/5-05/scripts/5-05_1.sh
@@ -1,6 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-PREFIX="${1:-NOT_SET}"
+ROOTUSER_NAME=root
+
+username=$(id -nu)
+if [ "$username" != "$ROOTUSER_NAME" ]; then
+    echo "Must be root to run \"$(basename "$0")\"."
+    exit 1
+fi
+
+trap 'echo "Ping exit (Ctrl-C)"; exit 1' 2
+
+OCTET_RANGE=({0..255})
+IP="$1"
+# TODO: ip regex validation
+
+tmp="$(cut -s -d. -f1,2 <<< "$IP")"
+PREFIX="${tmp:-NOT_SET}"
+
+tmp="$(cut -s -d. -f3 <<< "$IP")"
+SUBNET="${tmp:-${OCTET_RANGE[@]}}"
+
+tmp="$(cut -s -d. -f4 <<< "$IP")"
+HOST="${tmp:-${OCTET_RANGE[@]}}"
+
 INTERFACE="$2"
 
 [[ "$PREFIX" = "NOT_SET" ]] && {
@@ -13,11 +35,9 @@ if [[ -z "$INTERFACE" ]]; then
     exit 1
 fi
 
-for SUBNET in {1..255}
-do
-    for HOST in {1..255}
-    do
+for SUBNET in $SUBNET; do
+    for HOST in $HOST; do
         echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
-        arping -c 3 -I "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
+        arping-th -c 3 -i "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
     done
 done
